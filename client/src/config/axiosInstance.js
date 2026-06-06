3cbc5ca4f09964d945ea36058b05d888e8da3c30
elamaran-gh <elamaran1928@gmail.com>
2026-06-06 15:24:30 +0530

    initial commit

--- client/src/config/axiosInstance.js
+++ client/src/config/axiosInstance.js
@@ -1,8 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = import.meta.env.VITE_API_URL
-  ? `${import.meta.env.VITE_API_URL}/api/`
-  : "import.meta.env.VITE_API_URL || "http://localhost:6600"/api/";
+const BASE_URL = (import.meta.env.VITE_API_URL || "http://localhost:6600") + "/api/";
 
 const axiosInstance = axios.create();
 axiosInstance.defaults.baseURL = BASE_URL;
