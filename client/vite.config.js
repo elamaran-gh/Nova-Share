75f739bc1c34f8c1e49015f80f59a44b5f4d61fb
elamaran-gh <elamaran1928@gmail.com>
2026-06-08 17:51:49 +0530

    remove broken proxy

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,12 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api/files': 'import.meta.env.VITE_API_URL || "import.meta.env.VITE_API_URL || "http://localhost:6600""',
-    },
-  },
 })
